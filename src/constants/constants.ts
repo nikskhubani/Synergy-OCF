8641f811db0ac197b693d2c88f8da8035240e1e3
Cavdy <cavdyofficials@gmail.com>
2025-02-07 14:08:58 +0100

    constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,13 @@
+export const LocationTypeOptions = ['Residential High-Rise', 'Residential Low-Rise', 'Townhome Complex, Retail', 'Construction', 'Office Building', 'Office Complex', 'Industrial Park', 'Manufacturing', 'Healthcare', 'Education', 'Remote']
+
+export const ServiceOptions = ['Security Guard', 'Tactical Security', 'Concierge', 'Loss Prevention', 'Private Investigations', 'Alarm Response', 'Mobile Inspection', 'Parking Enforcement', 'Parking Management System', 'Other']
+
+export const PriceOptions = ['Regular Rate', 'Statutory Holiday Rate', 'Emergency Request Rate', 'Holiday Emergency Rate']
+
+export const BilledPerOptions = ['Hour', 'Instance', 'Other']
+
+export const InvoiceSubmissionOptions = ['ERP System', 'Email', 'Fax', 'Mail']
+
+export const TermsOptions = ['Pre-Authorized Debit (PAD)', 'Due on Receipt', 'Net 15 Days', 'Net 30 Days', 'Net 90 Days']
+
+export const NewAddressOptions = ["Head office address from this form", "Site Address from this form", "New Address"]
