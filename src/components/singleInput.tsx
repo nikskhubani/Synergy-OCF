64e90531416deaaa61b68f09c0324e5da4e814dc
Cavdy <cavdyofficials@gmail.com>
2025-02-07 14:08:41 +0100

    single input component

---
+++ src/components/singleInput.tsx
@@ -0,0 +1,37 @@
+import { Box, TextField, TextFieldProps } from '@mui/material';
+import { FormControl, InputLabel } from '../styles/global';
+
+type SingleInputProps = TextFieldProps & {
+  name?: string;
+  label: string;
+  isError?: boolean;
+  errorMessage?: string;
+};
+
+const SingleInput = ({ name, label, placeholder, type, isError, errorMessage, value, required, ...props }: SingleInputProps) => {
+  return (
+    <FormControl error={isError}>
+      <InputLabel required={required} aria-required={required} htmlFor={name} id={name}>
+        {label}
+      </InputLabel>
+      <Box sx={{display: 'flex', alignItems: 'center'}}>
+        <TextField
+          type={type}
+          fullWidth
+          size="small"
+          variant="outlined"
+          sx={{background: '#fff'}}
+          name={name}
+          id={name}
+          placeholder={placeholder}
+          value={value || ''}
+          {...props}
+          error={isError}
+          helperText={errorMessage}
+        />
+      </Box>
+    </FormControl>
+  );
+};
+
+export default SingleInput;
